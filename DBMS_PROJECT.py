6f6e988f8747d3e721bdf8cfa7a3e64f727f5a97
minhduc8386 <minhductrandz@gmail.com>
2025-05-17 01:59:41 +0700

    Add files via upload

--- DBMS_PROJECT.py
+++ DBMS_PROJECT.py
@@ -6,9 +6,9 @@ from datetime import datetime
 def get_connection():
     return mysql.connector.connect(
         host="localhost",
-        user="your_username",
-        password="your_password",
-        database="hotel_db"
+        user="root",
+        password="tmdcarryu29082005",
+        database="hotel"
     )
 
 # ----------- Operational Modules -----------
